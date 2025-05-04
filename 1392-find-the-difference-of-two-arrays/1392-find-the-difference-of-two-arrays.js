70c7bcc8609ff418f534f44f39ba4b00460274e9
Ansa P S <106726527+PSAnsa@users.noreply.github.com>
2025-05-04 20:39:15 +0300

    Time: 9 ms (66.93%), Space: 60.6 MB (80.92%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.js
@@ -4,22 +4,10 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    const set1 = new Set(nums1.map(Number));
-    const set2 = new Set(nums2.map(Number));
-    console.log(set1)
-    const distinctsum1 = [];
-    const distinctsum2 = [];
-    for (const num of set1){
-        if(!set2.has(num)){
-            distinctsum1.push(num)
-        }
-
-    }
-    for (const num of set2){
-        if(!set1.has(num)){
-            distinctsum2.push(num)
-        }
-    }
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const distinctsum1 = [...set1].filter(x=>!set2.has(x));
+    const distinctsum2 = [...set2].filter(x=>!set1.has(x));
 
     return [distinctsum1, distinctsum2]
     
