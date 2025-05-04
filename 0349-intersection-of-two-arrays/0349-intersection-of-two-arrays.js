bbe087c9166c42deda9e917bbbcfada758cd5c5c
Ansa P S <106726527+PSAnsa@users.noreply.github.com>
2025-05-04 21:02:33 +0300

    Time: 1 ms (82.68%), Space: 55.7 MB (23.48%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const intersect = [...set1].filter(x=>set2.has(x));
+    return intersect;
+};
