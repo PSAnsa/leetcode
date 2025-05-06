0380065b5a42523c0266f60e6f6267075b291353
Ansa P S <106726527+PSAnsa@users.noreply.github.com>
2025-05-07 01:55:43 +0300

    Time: 371 ms (23.29%), Space: 13.2 MB (37.56%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -1,8 +1,22 @@
 class Solution(object):
     def closeStrings(self, word1, word2):
-        frequency_word1 = Counter(word1)
-        frequency_word2 = Counter(word2)
-        sorted_value1 = sorted(frequency_word1.values())
-        sorted_value2  =  sorted(frequency_word2.values())
-        keys_match = set(frequency_word1.keys()) == set(frequency_word2.keys())
-        return sorted_value1 == sorted_value2 and keys_match
+        # frequency_word1 = Counter(word1)
+        # frequency_word2 = Counter(word2)
+        # sorted_value1 = sorted(frequency_word1.values())
+        # sorted_value2  =  sorted(frequency_word2.values())
+        # keys_match = set(frequency_word1.keys()) == set(frequency_word2.keys())
+        # return sorted_value1 == sorted_value2 and keys_match
+        
+        
+       
+        n1 = Counter(word1)
+        n2 = Counter(word2)
+        if set(n1.keys()) != set(n2.keys()):
+            return False
+
+       
+        if sorted(n1.values()) != sorted(n2.values()):
+            return False
+        
+        return True
+        
